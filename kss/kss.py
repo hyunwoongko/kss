c05c226368dd977ad391690c0775ad57c5201a9a
Hyunwoong Ko <gusdnd852@naver.com>
2020-12-13 00:09:41 +0900

    Update kss.py

--- kss/kss.py
+++ kss/kss.py
@@ -1,3 +1,12 @@
+# Korean Sentence Splitter
+# Split Korean text into sentences using heuristic algorithm.
+#
+# Copyright (C) 2019 Sang-Kil Park <skpark1224@hyundai.com> and Hyun-woong Ko <kevin.woong@kakaobrain.com>
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+
 from typing import List, Any
 from collections import defaultdict
 
