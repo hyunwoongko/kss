f05ac425cb9c4a698e3f9af60cf2a9f95fc1ede0
Hyunwoong Ko <gusdnd852@naver.com>
2020-12-12 23:46:31 +0900

    fix setup.py bug

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+def read_file(filename, cb):
+    with codecs.open(filename, 'r', 'utf8') as f:
+        return cb(f)
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='python-kss',
@@ -10,9 +20,22 @@ setup(
     description='Split Korean text into sentences using heuristic algorithm using pure python',
     long_description_content_type='text/markdown',
     platforms=['any'],
+    long_description=long_description,
+    packages=find_packages(exclude=[]),
+    keywords=['crawler', 'korean crawler', 'kochat'],
+    install_requires=read_file('requirements.txt', lambda f: list(
+        filter(bool, map(str.strip, f)))),
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ]
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
