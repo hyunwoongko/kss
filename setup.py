d138472dda11e84c668639699d253348b6441916
Hyunwoong Ko <gusdnd852@naver.com>
2020-12-13 00:15:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='python-kss',
-    version='0.0.1',
+    version='0.0',
     author='Hyunwoong Ko',
     author_email='kevin.woong@kakaobrain.com',
     url='https://github.com/hyunwoongko/kss-python',
