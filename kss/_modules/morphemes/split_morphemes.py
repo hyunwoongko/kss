4a6a32b590597b7ba8e292957ff30dbdbd92001a
Minho Ryu <ryu.minho@sk.com>
2023-05-17 11:57:26 +0900

    Add with_pos argument not to provide morphemes with pos

--- kss/_modules/morphemes/split_morphemes.py
+++ kss/_modules/morphemes/split_morphemes.py
@@ -18,6 +18,7 @@ def split_morphemes(
     backend: str = "auto",
     num_workers: Union[int, str] = "auto",
     drop_space: bool = True,
+    with_pos: bool = True,
 ) -> Union[List[Tuple[str, str]], List[List[Tuple[str, str]]], Union[List, Tuple]]:
     """
     Split texts into morphemes.
@@ -40,4 +41,10 @@ def split_morphemes(
 
     num_workers = _check_num_workers(text, num_workers)
     backend = _check_analyzer_backend(backend)
-    return _run_job(partial(backend.pos, drop_space=drop_space), text, num_workers)
+    result = _run_job(partial(backend.pos, drop_space=drop_space), text, num_workers)
+    if not with_pos:
+        if isinstance(text, str):
+            result = [token[0] for token in result]
+        else:
+            result = [[token[0] for token in tokens] for tokens in result]
+    return 
