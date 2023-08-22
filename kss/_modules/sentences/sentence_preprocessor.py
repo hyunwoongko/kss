03811e33d3ea912673bd352bbc5b430ce65351b7
hyunwoongko <hyunwoongko@gohyeon-ung-ui-MacBookPro.local>
2023-08-23 08:38:22 +0900

    Change email address

--- kss/_modules/sentences/sentence_preprocessor.py
+++ kss/_modules/sentences/sentence_preprocessor.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Hyunwoong Ko <kevin.ko@tunib.ai> and Sang-Kil Park <skpark1224@hyundai.com>
+# Copyright (C) 2021 Hyunwoong Ko <kevin.brain@kakaobrain.com> and Sang-Kil Park <skpark1224@hyundai.com>
 # All rights reserved.
 
 from typing import List, Tuple
---
+++ kss/_modules/summarization/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (C) 2021 Hyunwoong Ko <kevin.brain@kakaobrain.com> and Sang-Kil Park <skpark1224@hyundai.com>
+# All rights reserved.
